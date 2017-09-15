098cf1b467b411005dcec8a79b73437e7daee8be
jeongjoonsup <penny4860@gmail.com>
2017-09-15 10:51:10 +0900

    intensity_threshold() test

--- 2_thresholding.py
+++ 2_thresholding.py
@@ -3,12 +3,79 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from detector.cal import DistortionCorrector
 
-img = cv2.imread("test_images/straight_lines1.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-img = img[:,:,2]
+def intensity_threshold(image, thresh):
+    """
+    # Args
+        image : 3d array
+            RGB ordered image tensor
+        thresh : tuple
+            (minimun threshold, maximum threshold)
+    """
+    # Convert to HLS color space and separate the S channel
+    # Note: img is the undistorted image
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:,:,2]
+    binary = np.zeros_like(s_channel)
+    binary[(s_channel > thresh[0]) & (s_channel <= thresh[1])] = 1
+    return binary
 
-plt.imshow(img, cmap="gray")
+
+# 1. Distortion Correction
+corrector = DistortionCorrector.from_pkl("dataset//distortion_corrector.pkl")
+img = plt.imread('test_images/straight_lines1.jpg')
+img = corrector.run(img)
+
+binary = intensity_threshold(img, (128, 255))
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+ax1.imshow(img, cmap="gray")
+ax1.set_title('Original Image', fontsize=30)
+ax2.imshow(binary, cmap="gray")
+ax2.set_title('Thresholded Image', fontsize=30)
 plt.show()
 
+# # Convert to HLS color space and separate the S channel
+# # Note: img is the undistorted image
+# hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+# s_channel = hls[:,:,2]
+# 
+# # Grayscale image
+# # NOTE: we already saw that standard grayscaling lost color information for the lane lines
+# # Explore gradients in other colors spaces / color channels to see what might work better
+# gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+# 
+# # Sobel x
+# sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0) # Take the derivative in x
+# abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+# scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+# 
+# # Threshold x gradient
+# thresh_min = 20
+# thresh_max = 100
+# sxbinary = np.zeros_like(scaled_sobel)
+# sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+# 
+# # Threshold color channel
+# s_thresh_min = 170
+# s_thresh_max = 255
+# s_binary = np.zeros_like(s_channel)
+# s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+# 
+# # Stack each channel to view their individual contributions in green and blue respectively
+# # This returns a stack of the two binary images, whose components you can see as different colors
+# color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+# 
+# # Combine the two binary thresholds
+# combined_binary = np.zeros_like(sxbinary)
+# combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+# 
+# # Plotting thresholded images
+# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+# ax1.set_title('Stacked thresholds')
+# ax1.imshow(color_binary)
+# 
+# ax2.set_title('Combined S channel and gradient thresholds')
+# ax2.imshow(combined_binary, cmap='gray')
 
