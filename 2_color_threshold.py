21643581371472d00e04587642ed453b5fa688da
jeongjoonsup <penny4860@gmail.com>
2017-09-15 13:06:34 +0900

    gx, orientation, s-channel 조합으로 thresholding 완성

--- 2_color_threshold.py
+++ 2_color_threshold.py
@@ -13,20 +13,11 @@ if __name__ == "__main__":
     img = corrector.run(img)
     
     intensity_bin = Binarizer.intensity(img, (112, 255))
-    
-    plot_images([img, intensity_bin],
-                ["original", "s-channel"])
-
     gx_bin = Binarizer.gradient_x(img, (10, 255))
-    gy_bin = Binarizer.gradient_y(img, (10, 255))
-    grad_mag_bin = Binarizer.gradient_magnitude(img, (30, 255))
     grad_dir_bin = Binarizer.gradient_direction(img, (0.7, 1.3))
 
-    plot_images([img, gx_bin, grad_mag_bin, grad_dir_bin],
-                ["original", "gx", "mag", "thera"])
-
     output = np.zeros_like(gx_bin)
-    output[(gx_bin == 1) & (grad_dir_bin == 1)] = 1
+    output[(gx_bin == 1) & (grad_dir_bin == 1) | (intensity_bin == 1)] = 1
      
     plot_images([img, output],
                 ["original", "combined"])
