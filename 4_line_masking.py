cf4f5f91c40d79a1b1a559b5941a21be11d58005
jeongjoonsup <penny4860@gmail.com>
2017-09-18 21:20:16 +0900

    hough() 에서 각도에 따라 pruning

--- 4_line_masking.py
+++ 4_line_masking.py
@@ -6,22 +6,46 @@ from detector.binary import thresholding, plot_images
 from detector.warp import PerspectTrans
 import cv2
 import numpy as np
-
+import math
 
 def hough(binary, img):
+    
+    def _get_angle(x1, y1, x2, y2):
+        """Get angle to degree unit"""
+        dx = x2 - x1
+        dy = y2 - y1
+        return np.arctan2(dy, dx)*180/math.pi
+    
+    def _which_side(x1, y1, x2, y2, width):
+        if ((x1 + x2) / 2) < width / 2:
+            return "left"
+        else:
+            return "right"
+        
     # Hough Transform
-    minLineLength = 25
+    minLineLength = 5
     maxLineGap = 10
     lines = cv2.HoughLinesP(binary,
                             10,
                             np.pi/180,
-                            100,
+                            25,
                             np.array([]),
                             minLineLength=minLineLength,
                             maxLineGap=maxLineGap)
     for line in lines:
-        for x1,y1,x2,y2 in line:
-            cv2.line(img, (x1, y1), (x2, y2), color=(255,0,0), thickness=4)
+        x1, y1, x2, y2 = line[0]
+        theta = _get_angle(x1, y1, x2, y2)
+        
+        if _which_side(x1, y1, x2, y2, binary.shape[1]) == "left":
+            if theta <= -30 and theta >= -90:
+                cv2.line(img, (x1, y1), (x2, y2), color=(255,0,0), thickness=4)
+                # print("left", theta)
+                
+        else:
+            if theta >= 30 and theta <= 90:
+                cv2.line(img, (x1, y1), (x2, y2), color=(0,0,255), thickness=4)
+                # print("right", theta)
+
     return img
 
 if __name__ == "__main__":
