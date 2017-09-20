bb6517f057634f3621c9c43795008cab98caa436
jeongjoonsup <penny4860@gmail.com>
2017-09-20 19:46:04 +0900

    region_of_interest() 을 별도의 모듈에 이동

--- 5_lane_pixel.py
+++ 5_lane_pixel.py
@@ -2,44 +2,11 @@
 
 import matplotlib.pyplot as plt
 from detector.cal import DistortionCorrector
-from detector.binary import thresholding, plot_images, Binarizer, image_closing
+from detector.binary import thresholding, plot_images, Binarizer, image_closing, region_of_interest
 from detector.warp import PerspectTrans
 import cv2
 import numpy as np
 import math
-
-
-def region_of_interest(img):
-    """
-    Applies an image mask.
-    
-    Only keeps the region of the image defined by the polygon
-    formed from `vertices`. The rest of the image is set to black.
-    """
-    ylength, xlength = img.shape[:2]
-    
-    vertices = np.array([[(0, ylength),
-                          (xlength/2-ylength/10, ylength*0.5),
-                          (xlength/2+ylength/10, ylength*0.5),
-                          (xlength, ylength)]], dtype=np.int32)
-
-    
-    #defining a blank mask to start with
-    mask = np.zeros_like(img)   
-    
-    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
-    if len(img.shape) > 2:
-        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
-        ignore_mask_color = (255,) * channel_count
-    else:
-        ignore_mask_color = 255
-        
-    #filling pixels inside the polygon defined by "vertices" with the fill color    
-    cv2.fillPoly(mask, vertices, ignore_mask_color)
-    
-    #returning the image only where mask pixels are nonzero
-    masked_image = cv2.bitwise_and(img, mask)
-    return masked_image
 
 
 def run(img):
