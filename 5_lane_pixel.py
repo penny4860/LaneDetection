e9a0236190c3287a506e2753149d4b78dc5e41fc
jeongjoonsup <penny4860@gmail.com>
2017-09-20 19:42:41 +0900

    Revert "temp"
    This reverts commit 265455ce283cbe3ee743c2c5b22490bac0684cbd.

--- 5_lane_pixel.py
+++ 5_lane_pixel.py
@@ -40,25 +40,50 @@ def region_of_interest(img):
     #returning the image only where mask pixels are nonzero
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
-
-
-def run(image):
-    corrector = DistortionCorrector.from_pkl("dataset//distortion_corrector.pkl")
-
-    image = corrector.run(image)
-    edges = cv2.Canny(img,50,200)
-    image = region_of_interest(image)
-
-    binary_img = Binarizer.intensity(image, (78, 255))
-    binary_img = image_closing(binary_img)
     
-    combined = np.zeros_like(image)
-    combined[:,:,0] += edges
-    combined[:,:,2] += binary_img
-    combined = region_of_interest(combined)
+def hough(binary, img):
     
-    return binary_img, edges, combined
+    def _get_angle(x1, y1, x2, y2):
+        """Get angle to degree unit"""
+        dx = x2 - x1
+        dy = y2 - y1
+        return np.arctan2(dy, dx)*180/math.pi
     
+    def _which_side(x1, y1, x2, y2, width):
+        if ((x1 + x2) / 2) < width / 2:
+            return "left"
+        else:
+            return "right"
+        
+    # Hough Transform
+    minLineLength = 15
+    maxLineGap = 25
+    n_lines_thd = 15
+    pixel_unit = 1
+    theta_unit = np.pi/180
+    lines = cv2.HoughLinesP(binary,
+                            pixel_unit,
+                            theta_unit,
+                            n_lines_thd,
+                            np.array([]),
+                            minLineLength=minLineLength,
+                            maxLineGap=maxLineGap)
+    
+    output = np.zeros_like(img)
+    
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        theta = _get_angle(x1, y1, x2, y2)
+        
+        if _which_side(x1, y1, x2, y2, binary.shape[1]) == "left":
+            if theta <= -20 and theta >= -90:
+                cv2.line(output, (x1, y1), (x2, y2), color=(255,0,0), thickness=1)
+                
+        else:
+            if theta >= 20 and theta <= 90:
+                cv2.line(output, (x1, y1), (x2, y2), color=(0,0,255), thickness=1)
+
+    return output
 
 if __name__ == "__main__":
     # 1. Distortion Correction
@@ -69,7 +94,19 @@ if __name__ == "__main__":
     for filename in files[3:]:
         img = plt.imread(filename)
         
-        binary_img, edges, combined = run(img)
+        corrector = DistortionCorrector.from_pkl("dataset//distortion_corrector.pkl")
+
+        img = corrector.run(img)
+        edges = cv2.Canny(img,50,200)
+        img = region_of_interest(img)
+    
+        binary_img = Binarizer.intensity(img, (78, 255))
+        binary_img = image_closing(binary_img)
+        
+        combined = np.zeros_like(img)
+        combined[:,:,0] += edges
+        combined[:,:,2] += binary_img
+        combined = region_of_interest(combined)
         
         plot_images([img, binary_img, edges, combined],
                     ["original : {}".format(filename), "binary", "edges", "combined"])
