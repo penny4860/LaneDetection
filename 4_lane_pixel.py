e50db792b952073f5f459cb92bd6be8cd57bc119
jeongjoonsup <penny4860@gmail.com>
2017-09-22 15:16:34 +0900

    driver 정리

--- 4_lane_pixel.py
+++ 4_lane_pixel.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
-from detector.lane import run
+from detector.lane import LanePixelDetector
 from detector.cal import DistortionCorrector
 from detector.binary import plot_images
 import cv2
 import numpy as np
 
+from detector.binary import Binarizer, region_of_interest, image_closing
 
 np.set_printoptions(linewidth=500000)
 
@@ -15,14 +16,20 @@ if __name__ == "__main__":
     
     # 1. Distortion Correction
     corrector = DistortionCorrector.from_pkl("dataset//distortion_corrector.pkl")
+    detector = LanePixelDetector()
      
     import glob
     files = glob.glob('test_images//straight_lines1.jpg')
     for filename in files:
         img = plt.imread(filename)
-        img, binary_img, edges, combined, lane_map = run(img)
+        img = corrector.run(img)
+        
+        edges = cv2.Canny(img,50,200)
+        binary_img = Binarizer.intensity(region_of_interest(img), (48, 255))
+        binary_img = image_closing(binary_img)
 
-        plot_images([img, combined, lane_map],
-                    ["original : {}".format(filename), "combined", "lane_map"])
+        lane_map = detector.run(edges, binary_img)
 
+        plot_images([img, lane_map],
+                    ["original : {}".format(filename), "lane_map"])
 
