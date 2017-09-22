04f99eb04bcc9ac3c682f7306a5f6ac5bfd4506c
jeongjoonsup <penny4860@gmail.com>
2017-09-22 15:50:04 +0900

    class 로 정리

--- detector/binary.py
+++ detector/binary.py
@@ -4,23 +4,124 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def region_of_interest(img):
-    """
-    Applies an image mask.
-    
-    Only keeps the region of the image defined by the polygon
-    formed from `vertices`. The rest of the image is set to black.
-    """
-    ylength, xlength = img.shape[:2]
-    
-    vertices = np.array([[(0, ylength),
-                          (xlength/2-ylength/10, ylength*0.5),
-                          (xlength/2+ylength/10, ylength*0.5),
-                          (xlength, ylength)]], dtype=np.int32)
 
+class _Binarizer(object):
+    def __init__(self):
+        pass
     
+    def run(self, image, thresh):
+        pass
+
+    def _to_binaray(self, image, threshold=(0, 255)):
+        """
+        # Args
+            image : 2d array
+                uint8-scaled image
+
+            threshold : tuple
+        
+        # Returns
+            binary : 2d array
+                whose intensity is 0 or 1
+        """
+        binary = np.zeros_like(image)
+        binary[(image > threshold[0]) & (image <= threshold[1])] = 1
+        return binary
+
+    def _to_uint8_scale(self, image):
+        # Scale to 8-bit (0 - 255) then convert to type = np.uint8
+        image = np.uint8(255*image/np.max(image))
+        return image
+
+
+class SchannelBin(_Binarizer):
+    def run(self, image, thresh):
+        """
+        # Args
+            image : 3d array
+                RGB ordered image tensor
+            thresh : tuple
+                (minimun threshold, maximum threshold)
+        # Return
+            binary : 2d array
+                Binary image
+        """
+        # Convert to HLS color space and separate the S channel
+        # Note: img is the undistorted image
+        s_channel = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)[:,:,2]
+        binary = self._to_binaray(s_channel, thresh) * 255
+        return binary
+
+
+class GradientMagBin(_Binarizer):
+    def run(self, image, thresh=(0, 255)):
+        
+        sobel_kernel=3
+        # 1) Convert to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        
+        # 2) Take the gradient in x and y separately
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    
+        # 3) Calculate the magnitude 
+        sobel = np.sqrt(sobelx**2 + sobely**2)
+        sobel = self._to_uint8_scale(sobel)
+
+        binary = self._to_binaray(sobel, thresh)
+        return binary
+
+class GradientDirBin(_Binarizer):
+    def run(self, img, thresh=(0, np.pi/2)):
+        
+        sobel_kernel=3
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+        sobelx = np.absolute(sobelx)
+
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+        sobely = np.absolute(sobely)
+
+        sobel = np.arctan2(sobely, sobelx)
+        binary = self._to_binaray(sobel, thresh)
+        return binary
+
+
+class GxBin(_Binarizer):
+    def run(self, img, thresh=(0, 255)):
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        
+        # 2) Take the derivative in x or y given orient = 'x' or 'y'
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+        sobel = np.absolute(sobel)
+        sobel = self._to_uint8_scale(sobel)
+        binary = self._to_binaray(sobel, thresh)
+        return binary
+
+class GyBin(_Binarizer):
+    def run(self, img, thresh=(0, 255)):
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray,(5,5),0)
+        
+        # 2) Take the derivative in x or y given orient = 'x' or 'y'
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+        sobel = np.absolute(sobel)
+        sobel = self._to_uint8_scale(sobel)
+        binary = self._to_binaray(sobel, thresh)
+        return binary
+
+
+def roi_mask(img, vertices=None):
     #defining a blank mask to start with
-    mask = np.zeros_like(img)   
+    mask = np.zeros_like(img)
+    
+    if vertices is None:
+        ylength, xlength = img.shape[:2]
+        vertices = np.array([[(0, ylength),
+                              (xlength/2-ylength/10, ylength*0.5),
+                              (xlength/2+ylength/10, ylength*0.5),
+                              (xlength, ylength)]], dtype=np.int32)
     
     #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
     if len(img.shape) > 2:
@@ -36,112 +137,8 @@ def region_of_interest(img):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
-# Todo : 구조 정리
-def thresholding(image, do_opening=False, do_closing=True):
-    intensity_bin = Binarizer.intensity(image, (96, 255))
-    gx_bin = Binarizer.gradient_x(image, (5, 255))
-
-    output = np.zeros_like(gx_bin)
-    output[(gx_bin == 1) & (intensity_bin == 1)] = 255
-    if do_opening:
-        output = image_opening(output)
-    if do_closing:
-        output = image_closing(output)
-    return output
 
 
-def _to_uint8_scale(image):
-    # Scale to 8-bit (0 - 255) then convert to type = np.uint8
-    image = np.uint8(255*image/np.max(image))
-    return image
-
-
-def _to_binaray(image, threshold=(0, 255)):
-    binary = np.zeros_like(image)
-    binary[(image > threshold[0]) & (image <= threshold[1])] = 1
-    return binary
-
-
-class Binarizer(object):
-    
-
-    @staticmethod
-    def intensity(image, thresh):
-        """
-        # Args
-            image : 3d array
-                RGB ordered image tensor
-            thresh : tuple
-                (minimun threshold, maximum threshold)
-        """
-        # Convert to HLS color space and separate the S channel
-        # Note: img is the undistorted image
-        hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-        s_channel = hls[:,:,2]
-#        _, binary = cv2.threshold(s_channel, thresh[0], thresh[1], cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        binary = _to_binaray(s_channel, thresh) * 255
-        return binary
-
-    @staticmethod
-    def gradient_x(img, thresh=(0, 255)):
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        gray = cv2.GaussianBlur(gray,(5,5),0)
-        
-        # 2) Take the derivative in x or y given orient = 'x' or 'y'
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
-        sobel = np.absolute(sobel)
-        sobel = _to_uint8_scale(sobel)
-        
-        binary = _to_binaray(sobel, thresh)
-        return binary
-
-    @staticmethod
-    def gradient_y(img, thresh=(0, 255)):
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        gray = cv2.GaussianBlur(gray,(5,5),0)
-        
-        # 2) Take the derivative in x or y given orient = 'x' or 'y'
-        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
-        sobel = np.absolute(sobel)
-        sobel = _to_uint8_scale(sobel)
-
-        binary = _to_binaray(sobel, thresh)
-        return binary
-
-    @staticmethod
-    def gradient_magnitude(img, thresh=(0, 255), sobel_kernel=3):
-        
-        # 1) Convert to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        
-        # 2) Take the gradient in x and y separately
-        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    
-        # 3) Calculate the magnitude 
-        sobel = np.sqrt(sobelx**2 + sobely**2)
-        sobel = _to_uint8_scale(sobel)
-
-        binary = _to_binaray(sobel, thresh)
-        
-        return binary
-
-    @staticmethod
-    def gradient_direction(img, thresh=(0, np.pi/2), sobel_kernel=3):
-        
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-        sobelx = np.absolute(sobelx)
-
-        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-        sobely = np.absolute(sobely)
-
-        sobel = np.arctan2(sobely, sobelx)
-        binary = _to_binaray(sobel, thresh)
-        return binary
-    
-    
 def image_opening(image, ksize=(3,3)):
     """
     # Args
