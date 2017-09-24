2665e9f4fe1afd9aa78d1ebdf59212028cc05e7f
jeongjoonsup <penny4860@gmail.com>
2017-09-24 22:46:08 +0900

    warp -> pers

--- detector/curve/pers.py
+++ detector/curve/pers.py
@@ -31,6 +31,7 @@ class Warper(object):
         warped = cv2.warpPerspective(image, self._Minv, (w, h), flags=cv2.INTER_LINEAR)
         return warped
 
+
 class LaneWarper(Warper):
     """Perform perspective transform to make a image to bird eye's view"""
 
@@ -48,6 +49,44 @@ class LaneWarper(Warper):
                                    (w-x_offset, y_offset)]).astype(np.float32)
         
         super(LaneWarper, self).__init__(src_points, dst_points)
+
+
+class LaneMarker(object):
+    def __init__(self, warper):
+        self._warper = warper
+    
+    def run(self, image, left_fit, right_fit, plot=False):
+        """
+        # Args
+            image : distortion corrected image
+        """
+        ploty, left_fitx, right_fitx = self._generate_pts(image.shape[0], left_fit, right_fit)
+
+        color_warp = np.zeros_like(image).astype(np.uint8)
+        # Recast the x and y points into usable format for cv2.fillPoly()
+        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+        pts = np.hstack((pts_left, pts_right))
+        
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+        
+        # Warp the blank back to original image space using inverse perspective matrix (Minv)
+        newwarp = self._warper.backward(color_warp)
+    
+        # Combine the result with the original image
+        result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
+        if plot:
+            plt.imshow(result)
+            plt.show()
+        return result
+
+    def _generate_pts(self, height, left_curve, right_curve):
+        ys = np.linspace(0, height-1, height)
+        left_xs = left_curve[0]*ys**2 + left_curve[1]*ys + left_curve[2]
+        right_xs = right_curve[0]*ys**2 + right_curve[1]*ys + right_curve[2]
+        return ys, left_xs, right_xs
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
