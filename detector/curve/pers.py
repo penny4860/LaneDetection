5b56cb573ed15c5b2b53124b8f7cc4128092b512
jeongjoonsup <penny4860@gmail.com>
2017-09-26 08:17:16 +0900

    bug 수정 중

--- detector/curve/pers.py
+++ detector/curve/pers.py
@@ -94,9 +94,11 @@ class LaneMarker(object):
 
     def get_lane_line_map(self, image):
         line_map = np.zeros((image.shape[0], image.shape[1])).astype(np.uint8)
-        if np.any(self.left_pixels[:, 1] < 0) or np.any(self.left_pixels[:, 1] > image.shape[1]):
+        if np.any(self.left_pixels[:, 0] < 0) or np.any(self.left_pixels[:, 0] > image.shape[1]):
             return None
-        
+        if np.any(self.right_pixels[:, 0] < 0) or np.any(self.right_pixels[:, 0] > image.shape[1]):
+            return None
+
         line_map[self.left_pixels[:, 1], self.left_pixels[:, 0]] = 255
         line_map[self.right_pixels[:, 1], self.right_pixels[:, 0]] = 255
         line_map = self._warper.backward(line_map)
