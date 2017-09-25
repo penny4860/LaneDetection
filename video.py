84eada1353322ba802e1f4c47b27520479d265ab
jeongjoonsup <penny4860@gmail.com>
2017-09-26 00:29:21 +0900

    video framework run

--- video.py
+++ video.py
@@ -5,14 +5,15 @@ imageio.plugins.ffmpeg.download()
 
 # Import everything needed to edit/save/watch video clips
 from moviepy.editor import VideoFileClip
-from detector.framework import ImageFramework
+from detector.framework import ImageFramework, VideoFramework
 
+video_framework = VideoFramework()
 def process_image(image):
-    img_framework = ImageFramework()
-    return img_framework.run(image)
+    return video_framework.run(image)
  
 white_output = 'project_video_result.mp4'
-clip1 = VideoFileClip("project_video.mp4").subclip(0, 3)
+clip1 = VideoFileClip("project_video.mp4").subclip(22, 30)
+# clip1 = VideoFileClip("project_video.mp4")
 white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
 white_clip.write_videofile(white_output, audio=False)
 
